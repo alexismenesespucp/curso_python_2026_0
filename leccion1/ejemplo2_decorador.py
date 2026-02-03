cc223d5edb49e91c508482dc883f4129225a80e1
Alexis Meneses <alexjose70@gmail.com>
2026-02-03 18:50:37 -0500

    agregando decoradores al ejemplo 2

--- leccion1/ejemplo2_decorador.py
+++ leccion1/ejemplo2_decorador.py
@@ -1,20 +1,43 @@
-USUARIO_AUTENTICADO = False
+usuarios = {"Pepito": "admin","juancito":"user", "pancracio":"guest"}
+usuario = ""
+funciones_permitidas_por_usuario = ("check_status")
 
 def require_autentication(func):
     def wrapper(*args,**kwargs):
-        result = func(*args,**kwargs)
-        return result
+        if usuarios[usuario] == "admin":
+            func(*args,**kwargs)
+            return True
+        elif usuarios[usuario] == "user":
+            if func.__name__ in funciones_permitidas_por_usuario:
+                func(*args,**kwargs)
+                return True
+        print(f" No se puede ejecutar la funcion: {func.__name__} con el usuario {usuario}")
+        return False
     return wrapper
 
 @require_autentication
 def eliminar_base_datos():
     print("Base de datos eliminada")
 
-print("Intento 1(No logueado)")
+@require_autentication
+def agregar_usuario(nombre):
+    print(f"Agregando usuario: {nombre}")
+
+@require_autentication
+def check_status():
+    print("Estado verificado")
+
+usuario = "Pepito"
 eliminar_base_datos()
 
-USUARIO_AUTENTICADO = True
+usuario = "juancito"
+agregar_usuario("donatela")
 
-print("Intento 2(Autenticado)")
+usuario = "Pepito"
+check_status()
 
-eliminar_base_datos()
+usuario = "juancito"
+check_status()
+
+usuario = "pancracio"
+check_status()
